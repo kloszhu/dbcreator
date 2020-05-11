d8d2ab9f2bd952810f8a1319c0f073e15b9ff2e0
KlossZhu <zhuxiong@kzspp.com>
2020-05-11 17:42:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,58 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+//完整引入
+import Vue from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App';
+import 'ant-design-vue/dist/antd.css';
+Vue.config.productionTip = false;
+
+Vue.use(Antd);
+
+/* eslint-disable no-new */
+// new Vue({
+//     el: '#app',
+//     components: { App },
+//     template: '<App/>',
+// });
+//runtime
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    // router,
+    // store,
+    render: h => h(App)
+}).$mount("#app")
+
+//#endregion
+
+//#endregion
+//#endregion
+//#region 局部引入 
+// import Vue from 'vue';
+// import { Button, message } from 'ant-design-vue';
+// import App from './App';
+
+// Vue.config.productionTip = false;
+
+// /* v1.1.2 */
+// Vue.component(Button.name, Button);
+// Vue.component(Button.Group.name, Button.Group);
+
+// /* v1.1.3+ 自动注册Button下组件，如Button.Group */
+// Vue.use(Button);
+
+// Vue.prototype.$message = message;
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>',
+// });
